a25f11324aee4868669276190385f6015cd2faac
Bryan Drewery <bryan@shatow.net>
2006-09-15 01:35:30 +0000

    * Add initial HashTable class * Tests for HashTable appear to be the same as the BinaryTree tests...

---
+++ lib/bdlib/tests/HashTableTest.cpp
@@ -0,0 +1,183 @@
+/* HashTableTest.c
+ *
+ * $Id$
+ */
+#include "HashTableTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION (HashTableTest);
+
+void HashTableTest :: setUp (void)
+{
+/*
+    // set up test environment (initializing objects)
+    a = new String();
+    b = new String("blah");
+    c = new String(*b);
+    d = new String(cstring);
+    e = new String(*d);
+    f = new String(cstring, 11);
+    g = new String('x');
+    h = new String(35);
+*/
+  a = new HashTable<int, String>();
+}
+
+void HashTableTest :: tearDown (void)
+{
+    // finally delete objects
+    delete a; 
+/*
+    delete b; delete c; delete d;
+    delete e; delete f; delete g; delete h;
+*/
+}
+
+void HashTableTest :: insertTest (void)
+{
+  CPPUNIT_ASSERT_EQUAL((size_t)0, a->size());
+  a->insert(1, "Blah");
+  CPPUNIT_ASSERT_EQUAL((size_t)1, a->size());
+  a->insert(1, "Blah");
+  CPPUNIT_ASSERT_EQUAL((size_t)1, a->size());
+  a->insert(2, "Blah");
+  CPPUNIT_ASSERT_EQUAL((size_t)2, a->size());
+  a->insert(8, "Blah");
+  CPPUNIT_ASSERT_EQUAL((size_t)3, a->size());
+  a->insert(4, "Blah");
+  CPPUNIT_ASSERT_EQUAL((size_t)4, a->size());
+}
+
+void HashTableTest :: containsTest (void)
+{
+  a->insert(1, "Blah");
+  a->insert(1, "Bleck");
+  a->insert(2, "Blah");
+  a->insert(8, "Blah");
+  a->insert(4, "Blah");
+  
+  CPPUNIT_ASSERT_EQUAL(true, a->contains(1));
+  CPPUNIT_ASSERT_EQUAL(false, a->contains(3));
+  CPPUNIT_ASSERT_EQUAL(true, a->contains(4));
+}
+
+void HashTableTest :: getValueTest (void)
+{
+  a->insert(1, "Blah");
+  a->insert(1, "Bleck");
+  a->insert(2, "Blah2");
+  a->insert(8, "Blah8");
+  a->insert(4, "Blah4");
+  
+  String result;
+
+  result = a->getValue(58);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+  
+  result = a->getValue(1);
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", result);
+
+  result = a->getValue(4);
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah4", result);
+
+  result = a->getValue(8);
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah8", result);
+}
+
+#ifdef DISABLED
+void HashTableTest :: getKeyTest (void)
+{
+  a->insert(1, "Blah");
+  a->insert(1, "Bleck");
+  a->insert(2, "Blah2");
+  a->insert(8, "Blah8");
+  a->insert(4, "Blah4");
+  
+  CPPUNIT_ASSERT_EQUAL(1, a->getKey("Blah"));
+  CPPUNIT_ASSERT_EQUAL(2, a->getKey("Blah2"));
+  CPPUNIT_ASSERT_EQUAL(8, a->getKey("Blah8"));
+  CPPUNIT_ASSERT_EQUAL(4, a->getKey("Blah4"));
+}
+#endif
+
+void HashTableTest :: removeTest (void)
+{
+  a->insert(1, "Blah");
+  a->insert(1, "Bleck");
+  a->insert(2, "Blah2");
+  a->insert(8, "Blah8");
+  a->insert(4, "Blah4");
+
+  a->remove(8);  
+
+  String result;
+
+  result = a->getValue(58);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+
+ 
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", a->getValue(1));
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah4", a->getValue(4));
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah2", a->getValue(2));
+  result = a->getValue(8);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+
+  CPPUNIT_ASSERT_EQUAL(false, a->remove(8));
+
+  a->remove(4);
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", a->getValue(1));
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah2", a->getValue(2));
+  result = a->getValue(4);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+
+  a->remove(2);
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", a->getValue(1));
+  result = a->getValue(2);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+
+  a->remove(1);
+  CPPUNIT_ASSERT_EQUAL(true, a->isEmpty());
+  result = a->getValue(1);
+  CPPUNIT_ASSERT_EQUAL(true, result.isEmpty());
+}
+
+void HashTableTest :: iterateTest (void)
+{
+  a->insert(1, "Blah");
+  a->insert(1, "Bleck");
+  a->insert(2, "Blah2");
+  a->insert(8, "Blah8");
+  a->insert(4, "Blah4");
+
+  HashTable<int, String>::iterator iter = a->begin();
+  while (iter.hasNext()) {
+//    int key = (int) iter.next();
+//    printf("%d\n", key);
+    String value = iter.next();
+//    printf("%d: %s\n", key, a->getValue(key));
+  }
+
+  iter = a->begin();
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah2", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah4", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah8", iter.next());
+
+  a->remove(4);
+  iter = a->begin();
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah2", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah8", iter.next());
+
+  a->remove(2);
+  iter = a->begin();
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", iter.next());
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah8", iter.next());
+
+  a->remove(8);
+  iter = a->begin();
+  CPPUNIT_ASSERT_STRING_EQUAL("Blah", iter.next());
+
+  a->remove(1);
+  iter = a->begin();
+  CPPUNIT_ASSERT_EQUAL(false, iter.hasNext());
+}
---
+++ lib/bdlib/tests/HashTableTest.h
@@ -0,0 +1,41 @@
+/* HashTableTest.h
+ *
+ * $Id$
+ */
+#ifndef _HASHTABLETEST_H
+#define _HASHTABLETEST_H 1
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "String.h"
+#include "HashTable.h"
+using namespace BDLIB_NS;
+
+class HashTableTest : public CPPUNIT_NS :: TestFixture
+{
+    CPPUNIT_TEST_SUITE (HashTableTest);
+    CPPUNIT_TEST (insertTest);
+    CPPUNIT_TEST (containsTest);
+//    CPPUNIT_TEST (getKeyTest);
+    CPPUNIT_TEST (getValueTest);
+    CPPUNIT_TEST (removeTest);
+    CPPUNIT_TEST (iterateTest);
+    CPPUNIT_TEST_SUITE_END ();
+
+    public:
+        void setUp (void);
+        void tearDown (void);
+
+    protected:
+        void insertTest(void);
+        void containsTest(void);
+//        void getKeyTest(void);
+        void getValueTest(void);
+        void removeTest(void);
+        void iterateTest(void);
+    private:
+        HashTable<int, String> *a, *b, *c, *d, *e, *f, *g, *h;
+	int k1, k2, k3, k4;
+	String v1, v2, v3, v4;
+};
+#endif /* !_HASHTABLETEST_H */
