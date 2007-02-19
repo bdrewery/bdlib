7897d70a66ecf5cca40dc56f28a382c164ae41fc
Bryan Drewery <bryan@shatow.net>
2007-02-19 04:02:32 +0000

    * Add license commenting into String/base

--- lib/bdlib/String.cpp
+++ lib/bdlib/String.cpp
@@ -1,17 +1,36 @@
-/* String.c
+/* String.cpp
  *
- * $Id$
+ * Copyright (C) Bryan Drewery
+ *
+ * This program is private and may not be distributed, modified
+ * or used without express permission of Bryan Drewery.
+ *
+ * THIS PROGRAM IS DISTRIBUTED WITHOUT ANY WARRANTY.
+ * IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ * ARISING OUT OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY
+ * DERIVATIVES THEREOF, EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE
+ * IS PROVIDED ON AN "AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE
+ * NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+ * MODIFICATIONS.
+ *
  */
+
+#ifndef lint
+static const char rcsid[] = "$Id$";
+#endif
+
 #include "String.h"
 #include <stdarg.h>
 #include "base64.h"
 //#include <memory>
 //#include <iostream>
 //using namespace std;
-
-#ifndef lint
-static const char rcsid[] = "$Id$";
-#endif
 
 BDLIB_NS_BEGIN
 
