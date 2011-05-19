a82c6f9a53cd724eccdd562ab2ba65e258d2812d
Bryan Drewery <bryan@shatow.net>
2011-05-19 14:06:59 -0500

    * Update documentation

---
+++ docs/html/navtree.js
@@ -0,0 +1,381 @@
+var NAVTREE =
+[
+  [ "BDLIB", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Test List", "test.html", null ],
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "bd::Array< T >", "classbd_1_1Array.html", null ],
+      [ "bd::ArrayRef< T >", "classbd_1_1ArrayRef.html", null ],
+      [ "bd::Base64", "classbd_1_1Base64.html", null ],
+      [ "bd::ClientSocket", "classbd_1_1ClientSocket.html", null ],
+      [ "bd::Crypto", "classbd_1_1Crypto.html", null ],
+      [ "bd::Hash< Array< T > >", "structbd_1_1Hash_3_01Array_3_01T_01_4_01_4.html", null ],
+      [ "bd::Hash< bool >", "structbd_1_1Hash_3_01bool_01_4.html", null ],
+      [ "bd::Hash< char >", "structbd_1_1Hash_3_01char_01_4.html", null ],
+      [ "bd::Hash< int >", "structbd_1_1Hash_3_01int_01_4.html", null ],
+      [ "bd::Hash< long >", "structbd_1_1Hash_3_01long_01_4.html", null ],
+      [ "bd::Hash< short >", "structbd_1_1Hash_3_01short_01_4.html", null ],
+      [ "bd::Hash< signed char >", "structbd_1_1Hash_3_01signed_01char_01_4.html", null ],
+      [ "bd::Hash< String >", "structbd_1_1Hash_3_01String_01_4.html", null ],
+      [ "bd::Hash< T * >", "structbd_1_1Hash_3_01T_01_5_01_4.html", null ],
+      [ "bd::Hash< unsigned char >", "structbd_1_1Hash_3_01unsigned_01char_01_4.html", null ],
+      [ "bd::Hash< unsigned int >", "structbd_1_1Hash_3_01unsigned_01int_01_4.html", null ],
+      [ "bd::Hash< unsigned long >", "structbd_1_1Hash_3_01unsigned_01long_01_4.html", null ],
+      [ "bd::Hash< unsigned short >", "structbd_1_1Hash_3_01unsigned_01short_01_4.html", null ],
+      [ "bd::Iterator< T >", "classbd_1_1Iterator.html", null ],
+      [ "bd::KeyValue< Key, Value >", "structbd_1_1KeyValue.html", null ],
+      [ "bd::List< T >", "classbd_1_1List.html", null ],
+      [ "bd::List< T >::Node", "structbd_1_1List_1_1Node.html", null ],
+      [ "bd::Queue< T >", "classbd_1_1Queue.html", null ],
+      [ "bd::QueueConstIterator< T >", "classbd_1_1QueueConstIterator.html", null ],
+      [ "bd::QueueIterator< T >", "classbd_1_1QueueIterator.html", null ],
+      [ "bd::ReferenceCountedArray< T >", "classbd_1_1ReferenceCountedArray.html", null ],
+      [ "bd::ReferenceCountedArray< T >::Cref", "classbd_1_1ReferenceCountedArray_1_1Cref.html", null ],
+      [ "bd::ServerSocket", "classbd_1_1ServerSocket.html", null ],
+      [ "bd::Slice< T >", "classbd_1_1Slice.html", null ],
+      [ "bd::Socket", "classbd_1_1Socket.html", null ],
+      [ "bd::sockname_t", "structbd_1_1sockname__t.html", null ],
+      [ "bd::SortedList< T >", "classbd_1_1SortedList.html", null ],
+      [ "bd::Stack< T >", "classbd_1_1Stack.html", null ],
+      [ "bd::Stream", "classbd_1_1Stream.html", null ],
+      [ "bd::String", "classbd_1_1String.html", null ],
+      [ "bd::Thread", "classbd_1_1Thread.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "bd::ArrayRef< value_type >", "classbd_1_1ArrayRef.html", null ],
+      [ "bd::ArrayRef< T >", "classbd_1_1ArrayRef.html", null ],
+      [ "bd::Base64", "classbd_1_1Base64.html", null ],
+      [ "bd::Crypto", "classbd_1_1Crypto.html", null ],
+      [ "bd::Hash< Array< T > >", "structbd_1_1Hash_3_01Array_3_01T_01_4_01_4.html", null ],
+      [ "bd::Hash< bool >", "structbd_1_1Hash_3_01bool_01_4.html", null ],
+      [ "bd::Hash< char >", "structbd_1_1Hash_3_01char_01_4.html", null ],
+      [ "bd::Hash< int >", "structbd_1_1Hash_3_01int_01_4.html", null ],
+      [ "bd::Hash< long >", "structbd_1_1Hash_3_01long_01_4.html", null ],
+      [ "bd::Hash< short >", "structbd_1_1Hash_3_01short_01_4.html", null ],
+      [ "bd::Hash< signed char >", "structbd_1_1Hash_3_01signed_01char_01_4.html", null ],
+      [ "bd::Hash< String >", "structbd_1_1Hash_3_01String_01_4.html", null ],
+      [ "bd::Hash< T * >", "structbd_1_1Hash_3_01T_01_5_01_4.html", null ],
+      [ "bd::Hash< unsigned char >", "structbd_1_1Hash_3_01unsigned_01char_01_4.html", null ],
+      [ "bd::Hash< unsigned int >", "structbd_1_1Hash_3_01unsigned_01int_01_4.html", null ],
+      [ "bd::Hash< unsigned long >", "structbd_1_1Hash_3_01unsigned_01long_01_4.html", null ],
+      [ "bd::Hash< unsigned short >", "structbd_1_1Hash_3_01unsigned_01short_01_4.html", null ],
+      [ "bd::Iterator< T >", "classbd_1_1Iterator.html", null ],
+      [ "bd::KeyValue< Key, Value >", "structbd_1_1KeyValue.html", null ],
+      [ "bd::List< T >", "classbd_1_1List.html", [
+        [ "bd::Queue< T >", "classbd_1_1Queue.html", null ],
+        [ "bd::SortedList< T >", "classbd_1_1SortedList.html", null ],
+        [ "bd::Stack< T >", "classbd_1_1Stack.html", null ]
+      ] ],
+      [ "bd::List< T >::Node", "structbd_1_1List_1_1Node.html", null ],
+      [ "bd::QueueConstIterator< T >", "classbd_1_1QueueConstIterator.html", null ],
+      [ "bd::QueueIterator< T >", "classbd_1_1QueueIterator.html", null ],
+      [ "bd::ReferenceCountedArray< T >", "classbd_1_1ReferenceCountedArray.html", [
+        [ "bd::Array< T >", "classbd_1_1Array.html", null ]
+      ] ],
+      [ "bd::ReferenceCountedArray< T >::Cref", "classbd_1_1ReferenceCountedArray_1_1Cref.html", null ],
+      [ "bd::Slice< T >", "classbd_1_1Slice.html", null ],
+      [ "bd::Socket", "classbd_1_1Socket.html", [
+        [ "bd::ClientSocket", "classbd_1_1ClientSocket.html", null ],
+        [ "bd::ServerSocket", "classbd_1_1ServerSocket.html", null ]
+      ] ],
+      [ "bd::sockname_t", "structbd_1_1sockname__t.html", null ],
+      [ "bd::Stream", "classbd_1_1Stream.html", null ],
+      [ "bd::Thread", "classbd_1_1Thread.html", null ],
+      [ "bd::Iterator< iterator_type >", "classbd_1_1Iterator.html", null ],
+      [ "bd::ReferenceCountedArray< String_Array_Type >", "classbd_1_1ReferenceCountedArray.html", [
+        [ "bd::String", "classbd_1_1String.html", null ]
+      ] ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "bd", "namespacebd.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "Array.cpp", "Array_8cpp.html", null ],
+      [ "Array.h", "Array_8h.html", null ],
+      [ "base64.c", "base64_8c.html", null ],
+      [ "base64.h", "base64_8h.html", null ],
+      [ "bdlib.h", "bdlib_8h.html", null ],
+      [ "BinaryTree.cpp", "BinaryTree_8cpp.html", null ],
+      [ "BinaryTree.h", "BinaryTree_8h.html", null ],
+      [ "bits.h", "bits_8h.html", null ],
+      [ "Crypto.cpp", "Crypto_8cpp.html", null ],
+      [ "Crypto.h", "Crypto_8h.html", null ],
+      [ "Cstrings.c", "Cstrings_8c.html", null ],
+      [ "Cstrings.h", "Cstrings_8h.html", null ],
+      [ "hash.h", "hash_8h.html", null ],
+      [ "HashTable.cpp", "HashTable_8cpp.html", null ],
+      [ "HashTable.h", "HashTable_8h.html", null ],
+      [ "Iterator.h", "Iterator_8h.html", null ],
+      [ "List.cpp", "List_8cpp.html", null ],
+      [ "List.h", "List_8h.html", null ],
+      [ "Queue.cpp", "Queue_8cpp.html", null ],
+      [ "Queue.h", "Queue_8h.html", null ],
+      [ "ReferenceCountedArray.cpp", "ReferenceCountedArray_8cpp.html", null ],
+      [ "ReferenceCountedArray.h", "ReferenceCountedArray_8h.html", null ],
+      [ "Socket.cpp", "Socket_8cpp.html", null ],
+      [ "Socket.h", "Socket_8h.html", null ],
+      [ "SortedList.cpp", "SortedList_8cpp.html", null ],
+      [ "SortedList.h", "SortedList_8h.html", null ],
+      [ "Stack.cpp", "Stack_8cpp.html", null ],
+      [ "Stack.h", "Stack_8h.html", null ],
+      [ "Stream.cpp", "Stream_8cpp.html", null ],
+      [ "Stream.h", "Stream_8h.html", null ],
+      [ "String.cpp", "String_8cpp.html", null ],
+      [ "String.h", "String_8h.html", null ],
+      [ "Thread.cpp", "Thread_8cpp.html", null ],
+      [ "Thread.h", "Thread_8h.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
