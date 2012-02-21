a9ffe842d1277ec9ffec00b0a78e5dee82c64278
Bryan Drewery <bryan@shatow.net>
2012-02-20 19:41:12 -0600

    * Don't auto enable caching in configure

--- build/autotools/fixconfigure.sh
+++ build/autotools/fixconfigure.sh
@@ -9,8 +9,6 @@ if [ $? -eq 1 ]; then
 fi
 
 if $($SED $FILE \
--e "s/^cache_file=\/dev\/null/cache_file=\.\/build\/config.cache/" \
--e "s/&& echo \"updating cache .cache_file\"//" \
 -e "s/confdefs\.h/build\/confdefs\.h/" \
 > $TMPNAM); then
   mv -f $TMPNAM $1
