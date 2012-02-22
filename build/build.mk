7ba71d163aa9b9ab8d9db03e3fd4bf0293878972
Bryan Drewery <bryan@shatow.net>
2012-02-22 14:53:01 -0600

    * Avoid needing `basename`

--- build/build.mk
+++ build/build.mk
@@ -4,13 +4,13 @@ BDLIB_UNITS = $(top_srcdir)/src/libbdlib_units.a
 depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 
 %.o : %.cpp
-#	@echo -e "[+] Compiling: \033[1m$(basename $< .cpp)\033[0m"
+#	@echo -e "[+] Compiling: \033[1m$*\033[0m"
 	@echo -e "[CC]	$<"
 	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 %.o : %.c
-#	@echo -e "[+] Compiling: \033[1m$(basename $< .c)\033[0m"
+#	@echo -e "[+] Compiling: \033[1m$*\033[0m"
 	@echo -e "[CC]	$<"
 	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
 	$(CXX) $(CXXFLAGS) -c $< -o $@
