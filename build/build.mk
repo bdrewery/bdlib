d361daf0cfdd41a8cff2e70219bccacf403c7cc0
Bryan Drewery <bryan@shatow.net>
2012-02-22 14:57:23 -0600

    * Inline the dependency checking to avoid depcomp

--- build/build.mk
+++ build/build.mk
@@ -6,12 +6,25 @@ depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 %.o : %.cpp
 #	@echo -e "[+] Compiling: \033[1m$*\033[0m"
 	@echo -e "[CC]	$<"
+ifeq ($(CCDEPMODE),gcc3)
+	if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+	mv '.deps/$*.TPo' '.deps/$*.Po'; \
+	else rm -f '.deps/$*.Tpo'; exit 1; \
+	fi
+else
 	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
 	$(CXX) $(CXXFLAGS) -c $< -o $@
+endif
 
 %.o : %.c
 #	@echo -e "[+] Compiling: \033[1m$*\033[0m"
 	@echo -e "[CC]	$<"
+ifeq ($(CCDEPMODE),gcc3)
+	if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+	mv '.deps/$*.TPo' '.deps/$*.Po'; \
+	else rm -f '.deps/$*.Tpo'; exit 1; \
+	fi
+else
 	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
 	$(CXX) $(CXXFLAGS) -c $< -o $@
-
+endif
