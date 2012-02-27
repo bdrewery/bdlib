27bde51e9fed1a7e1f9ac36b0a3957a364a2471a
Bryan Drewery <bryan@shatow.net>
2012-02-26 22:47:33 -0600

    * Improve make portability

--- build/build.mk
+++ build/build.mk
@@ -3,28 +3,28 @@ BDLIB_UNITS = $(top_srcdir)/src/libbdlib_units.a
 
 depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 
-%.o : %.cpp
-#	@echo -e "[+] Compiling: \033[1m$*\033[0m"
-	@echo -e "[CC]	$<"
-ifeq ($(CCDEPMODE),gcc3)
-	if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
-	mv '.deps/$*.TPo' '.deps/$*.Po'; \
-	else rm -f '.deps/$*.Tpo'; exit 1; \
-	fi
-else
-	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
-	$(CXX) $(CXXFLAGS) -c $< -o $@
-endif
+.SUFFIXES: .c .cpp .h .o
 
-%.o : %.c
-#	@echo -e "[+] Compiling: \033[1m$*\033[0m"
+.cpp.o:
 	@echo -e "[CC]	$<"
-ifeq ($(CCDEPMODE),gcc3)
-	if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
-	mv '.deps/$*.TPo' '.deps/$*.Po'; \
-	else rm -f '.deps/$*.Tpo'; exit 1; \
-	fi
-else
-	source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
-	$(CXX) $(CXXFLAGS) -c $< -o $@
-endif
+	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
+		if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+		mv '.deps/$*.TPo' '.deps/$*.Po'; \
+		else rm -f '.deps/$*.Tpo'; exit 1; \
+		fi; \
+	else \
+		source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
+		$(CXX) $(CXXFLAGS) -c $< -o $@; \
+	fi;
+
+.c.o:
+	@echo -e "[C ]	$<"
+	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
+		if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+		mv '.deps/$*.TPo' '.deps/$*.Po'; \
+		else rm -f '.deps/$*.Tpo'; exit 1; \
+		fi; \
+	else \
+		source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
+		$(CXX) $(CXXFLAGS) -c $< -o $@; \
+	fi;
