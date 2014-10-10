bde188534a9ce4eadb11bd02407d07823c060e96
Bryan Drewery <bryan@shatow.net>
2014-10-09 20:22:39 -0500

    Cleanup use of CPPFLAGS

--- build/build.mk
+++ build/build.mk
@@ -9,23 +9,23 @@ depcomp = /bin/sh $(top_srcdir)/build/autotools/depcomp
 .cpp.o:
 	@echo -e "[CC]	$<"
 	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
-		if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+		if $(CXX) $(CPPFLAGS) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
 		mv '.deps/$*.TPo' '.deps/$*.Po'; \
 		else rm -f '.deps/$*.Tpo'; exit 1; \
 		fi; \
 	else \
 		source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
-		$(CXX) $(CXXFLAGS) -c $< -o $@; \
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@; \
 	fi;
 
 .c.o:
 	@echo -e "[C ]	$<"
 	if [ "$(CCDEPMODE)" = "gcc3" ]; then \
-		if $(CXX) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
+		if $(CXX) $(CPPFLAGS) -MT '$@' -MD -MP -MF '.deps/$*.TPo' $(CXXFLAGS) -c $< -o $@; then \
 		mv '.deps/$*.TPo' '.deps/$*.Po'; \
 		else rm -f '.deps/$*.Tpo'; exit 1; \
 		fi; \
 	else \
 		source='$<' object='$@' depfile='.deps/$*.Po' tmpdepfile='.deps/$*.TPo' depmode=$(CCDEPMODE) $(depcomp) \
-		$(CXX) $(CXXFLAGS) -c $< -o $@; \
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@; \
 	fi;
